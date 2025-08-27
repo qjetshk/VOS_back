3c113bd1f879185efa904cc6c0d2a006bf63e3c9
Антон <vash_email@email.com>
2025-08-27 14:04:18 +0500

    fine-tuning

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.VERCEL_URL || "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
