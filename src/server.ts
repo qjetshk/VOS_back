5ba70c7470d8582ca2313c2d354a18f253aea802
Антон <vash_email@email.com>
2025-08-27 19:32:07 +0500

    fixing the cors issue

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.DEPLOY_VERCEL_URL || "http://localhost:5173",
+    origin: /* process.env.DEPLOY_VERCEL_URL */ 'https://vos-front.vercel.app',
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   })
